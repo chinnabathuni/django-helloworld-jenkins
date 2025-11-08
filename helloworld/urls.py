1e759f0130776f1fb5f5b072d791285b06ed2303
lokesh chinnabathuni <lokeshchinnabathuni@lokeshs-MacBook-Air.local>
2025-11-08 12:01:43 -0600

    Add index view to fix import error

--- helloworld/urls.py
+++ helloworld/urls.py
@@ -1,4 +1,8 @@
+from django.contrib import admin
+from django.urls import path
 from webapp.views import index
+
 urlpatterns = [
-    path('', index, name='home'),
+    path('admin/', admin.site.urls),
+    path('', index, name='index'),
 ]
