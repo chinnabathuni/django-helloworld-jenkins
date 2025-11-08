d6ff2f21d338ba29a7578197d05d9e1be5dd1450
lokesh chinnabathuni <lokeshchinnabathuni@lokeshs-MacBook-Air.local>
2025-11-08 11:58:16 -0600

    Test auto trigger from GitHub

--- webapp/views.py
+++ webapp/views.py
@@ -1,8 +1,6 @@
 
-# Create your views here.
 from django.shortcuts import render
-from datetime import datetime
 
-def home(request):
-    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    return render(request, "index.html", {"time": current_time})
+def index(request):
+    return render(request, 'webapp/templates/index.html')
+
